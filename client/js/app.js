fbbaf0b22b8a8ea7aabda5ee4e860010a7c67e25
Davis Kim <dchk90@gmail.com>
2014-05-16 16:50:41 -0700

    Add routing for home and shorten

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,23 @@
-angular.module('app', [])
-.controller('MyController', function($scope) {
-  $scope.person = {
-    name: 'something'
-  };
-});
+var angular = angular;
+
+angular.module('app', ['ngRoute'])
+.controller('HomeController', function($scope) {
+  $scope.user = 'davis';
+})
+.controller('ShortenController', function($scope) {
+  $scope.user = 'jules';
+})
+
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'HomeController',
+      templateUrl: '/client/templates/home.html'
+    })
+    .when('/shorten', {
+      controller: 'ShortenController',
+      templateUrl: '/client/templates/shorten.html'
+    })
+    .otherwise({redirectTo: '/'});
+  $locationProvider.html5Mode(true);
+}]);
