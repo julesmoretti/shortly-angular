977f15e46c2f4414b79fde5ff4761743bcc662cb
Davis Kim <dchk90@gmail.com>
2014-05-16 19:05:23 -0700

    Add semicolon

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoUrl = process.env.MONGO_LABS_URL || 'mongodb://localhost/localshortly'
+var mongoUrl = process.env.MONGO_LABS_URL || 'mongodb://localhost/localshortly';
 var db = mongoose.connect(mongoUrl, function() {
   console.log('connected to mango', mongoUrl);
 });
