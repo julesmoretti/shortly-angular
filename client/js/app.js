7923e6b4bb0d0a8347eca29309772511b66561d0
Davis Kim <dchk90@gmail.com>
2014-05-17 18:30:44 -0700

    Saturday night

--- client/js/app.js
+++ client/js/app.js
@@ -9,7 +9,7 @@ angular.module('app', ['ngRoute'])
 .controller('ShortenController', function($scope, $http, $location) {
   $scope.link = 'http://google.com';
   // $scope.spinnerStatus = false;
-  $scope.saveLink = function() {
+  $scope.submit = function() {
     $scope.spinnerToggle();
     $http.post('/links', {url: $scope.link})
       .success(function() {
@@ -26,17 +26,66 @@ angular.module('app', ['ngRoute'])
     // show preview
   };
 })
+.controller('LoginController', function($scope, $http, $window) {
+  $scope.user = {username: $scope.username, password: $scope.password};
+  $scope.message = '';
+  $scope.submit = function() {
+    $http
+      .post('/login', $scope.user)
+      .success(function (data, status, headers, config) {
+        $window.sessionStorage.token = data.token;
+        $scope.message = 'Welcome';
+      })
+      .error(function (data, status, headers, config) {
+        // Erase the token if the user fails to log in
+        delete $window.sessionStorage.token;
+
+        // Handle login errors here
+        $scope.message = 'Error: Invalid user or password';
+      });
+  };
+
+})
+.controller('SignupController', function($scope, $http, $window){
+  $scope.message = '';
+  $scope.submit = function() {
+    $http.post('/signup', {username: $scope.username, password: $scope.password})
+      .success(function (data, status, headers, config) {
+        $window.sessionStorage.token = data.token;
+        $scope.message = 'Welcome';
+      })
+      .error(function (data, status, headers, config) {
+        // Erase the token if the user fails to log in
+        delete $window.sessionStorage.token;
+
+        // Handle login errors here
+        $scope.message = 'Error: Invalid user or password';
+      });
+  };
+})
 
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       controller: 'HomeController',
-      templateUrl: '/client/templates/home.html'
+      templateUrl: '/client/templates/home.html',
+      access: 'access.loggedin'
     })
     .when('/shorten', {
       controller: 'ShortenController',
-      templateUrl: '/client/templates/shorten.html'
+      templateUrl: '/client/templates/shorten.html',
+      access: 'access.loggedin'
     })
-    .otherwise({redirectTo: '/'});
+    .when('/login', {
+      controller: 'LoginController',
+      templateUrl: '/client/templates/login.html',
+      access: 'access.public'
+    })
+    .when('/signup', {
+      controller: 'SignupController',
+      templateUrl: '/client/templates/signup.html',
+      access: 'access.public'
+    })
+    .otherwise({redirectTo: '/login'});
   $locationProvider.html5Mode(true);
 }]);
--- server-config.js
+++ server-config.js
@@ -6,6 +6,11 @@ var handler = require('./lib/request-handler');
 
 var app = express();
 
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+
+
+
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -15,6 +20,13 @@ app.configure(function() {
   app.use('/client', express.static(__dirname + '/client'));
   app.use(express.static(__dirname));
   app.use(express.cookieParser('shhhh, very secret'));
+
+  // We are going to protect /api routes with JWT
+  app.use('/api', expressJwt({secret: 'secret'}));
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+
   app.use(express.session());
 });
 
@@ -24,14 +36,14 @@ app.get('/shorten', handler.renderIndex);
 app.get('/links', handler.fetchLinks);
 app.post('/links', handler.saveLink);
 
-app.get('/login', handler.loginUserForm);
+app.get('/login', handler.renderIndex);
 app.post('/login', handler.loginUser);
 app.get('/logout', handler.logoutUser);
 
 // ===== dev stage test =====
 app.get('/findusers', handler.findUsers);
 
-app.get('/signup', handler.signupUserForm);
+app.get('/signup', handler.renderIndex);
 app.post('/signup', handler.signupUser);
 
 app.get('/*', handler.navToLink);
